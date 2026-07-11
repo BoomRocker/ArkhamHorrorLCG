927f164e267856242da37023dafe8a3cb2de783e
BoomRocker <boomrocker@gmail.com>
2026-07-11 04:31:47 -0500

    changes
    change

--- older/sw.js
+++ older/sw.js
@@ -1,32 +1,33 @@
-const CACHE="arkham-v1"
+const CACHE_NAME = 'arkham-companion-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest'
+];
 
-const FILES=[
-"./",
-"./index.html",
-"./style.css",
-"./app.js"
-]
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
 
-self.addEventListener("install",e=>{
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
 
-e.waitUntil(
-
-caches.open(CACHE)
-
-.then(cache=>cache.addAll(FILES))
-
-)
-
-})
-
-self.addEventListener("fetch",e=>{
-
-e.respondWith(
-
-caches.match(e.request)
-
-.then(r=>r||fetch(e.request))
-
-)
-
-})
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
+});
